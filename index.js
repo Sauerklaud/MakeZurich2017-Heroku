d013e0ffa86cbbf46438eb76da9dc5701221fcf2
darior <dario.roethlisberger@outlook.com>
2017-02-03 14:50:15 +0100

    added mongodb

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ var ttn = require('ttn');
 
 var appId = 'sauerklaud';
 var accessKey = 'ttn-account-v2.AAQToobiTgVh3BGgVUaaY437KaBL8J9HkuM5qEnbcMA';
-
 var client = new ttn.Client('eu', appId, accessKey);
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp');
 
 client.on('message', function(deviceId, message) {
   console.log(message.payload_raw);
+
+  var collection = db.get('airquality');
+  collection.insert({
+    "raw" : message.payload_raw.toString(),
+  }, function (err, doc) {
+  if (err) {
+    console.log('Insert DB Error!');
+  } 
+  });
 });
 
 var path = require('path')
@@ -28,6 +40,7 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
 
