03dae53552bde0d8d05911a014e40a737edb6dfa
darior <dario.roethlisberger@outlook.com>
2017-02-04 14:26:09 +0100

    changed color

--- public/javascript/rainbow.js
+++ public/javascript/rainbow.js
@@ -7,8 +7,8 @@ function Rainbow()
 {
 	"use strict";
 	var gradients = null;
-	var minNum = 50;
-	var maxNum = 300;
+	var minNum = 30;
+	var maxNum = 150;
 	var colours = ['ff0000', 'ffff00', '00ff00', '0000ff']; 
 	setColours(colours);
 	
